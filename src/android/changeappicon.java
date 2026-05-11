5b659f6d8c1c72e0680b804efd5f3e0ea3ce2483
harikrir <hkrishnan254@gmail.com>
2026-05-11 14:38:02 +0300

    Add files via upload

---
+++ src/android/changeappicon.java
@@ -0,0 +1,95 @@
+package com.cordova.changeappicon;
+
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.pm.PackageManager;
+
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CallbackContext;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
+/**
+ * Cordova Plugin: Change Android Launcher Icon
+ *
+ * Supported icons:
+ *  - light
+ *  - dark
+ *  - private
+ *
+ * NOTE:
+ * Activity-alias entries MUST be injected via plugin.xml
+ */
+public class changeappicon extends CordovaPlugin {
+
+    private static final String ACTION_CHANGE_ICON = "changeIcon";
+
+    @Override
+    public boolean execute(
+            String action,
+            JSONArray args,
+            CallbackContext callbackContext
+    ) throws JSONException {
+
+        if (!ACTION_CHANGE_ICON.equals(action)) {
+            callbackContext.error("Unsupported action: " + action);
+            return false;
+        }
+
+        if (args == null || args.length() == 0) {
+            callbackContext.error("Missing icon name");
+            return true;
+        }
+
+        String iconName = args.getString(0); // light | dark | private
+        changeIcon(iconName, callbackContext);
+        return true;
+    }
+
+    private void changeIcon(String iconName, CallbackContext callbackContext) {
+        try {
+            Context context = cordova.getActivity();
+            PackageManager pm = context.getPackageManager();
+            String packageName = context.getPackageName();
+
+            // Disable MAIN launcher activity
+            ComponentName mainActivity =
+                new ComponentName(context, context.getClass());
+
+            pm.setComponentEnabledSetting(
+                mainActivity,
+                PackageManager.COMPONENT_ENABLED_STATE_DISABLED,
+                PackageManager.DONT_KILL_APP
+            );
+
+            // Enable/disable activity aliases
+            setAlias(pm, packageName, "Light", "light".equals(iconName));
+            setAlias(pm, packageName, "Dark", "dark".equals(iconName));
+            setAlias(pm, packageName, "Private", "private".equals(iconName));
+
+            callbackContext.success("Icon changed to: " + iconName);
+
+        } catch (Exception e) {
+            callbackContext.error("Icon change failed: " + e.getMessage());
+        }
+    }
+
+    private void setAlias(
+            PackageManager pm,
+            String packageName,
+            String alias,
+            boolean enable
+    ) {
+        ComponentName component =
+            new ComponentName(packageName, packageName + "." + alias);
+
+        pm.setComponentEnabledSetting(
+            component,
+            enable
+                ? PackageManager.COMPONENT_ENABLED_STATE_ENABLED
+                : PackageManager.COMPONENT_ENABLED_STATE_DISABLED,
+            PackageManager.DONT_KILL_APP
+        );
+    }
+}
