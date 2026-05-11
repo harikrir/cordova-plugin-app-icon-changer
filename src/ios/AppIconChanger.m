424746ebf7992d276b870323175821bfa73c7ae4
harikrir <hkrishnan254@gmail.com>
2026-05-11 09:33:36 +0300

    Update AppIconChanger.m

--- src/ios/AppIconChanger.m
+++ src/ios/AppIconChanger.m
@@ -1,59 +1,57 @@
 #import "Cordova/CDV.h"
 #import "AppIconChanger.h"
-
 @implementation AppIconChanger
-
-- (void) isSupported:(CDVInvokedUrlCommand*)command
+- (void)isSupported:(CDVInvokedUrlCommand*)command
 {
-  CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsInt:[self supportsAlternateIcons]];
-  [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+   BOOL supported = NO;
+   if (@available(iOS 10.3, *)) {
+       supported = [[UIApplication sharedApplication] supportsAlternateIcons];
+   }
+   CDVPluginResult* result =
+   [CDVPluginResult resultWithStatus:CDVCommandStatus_OK
+                        messageAsBool:supported];
+   [self.commandDelegate sendPluginResult:result
+                               callbackId:command.callbackId];
 }
-
-- (void) changeIcon:(CDVInvokedUrlCommand*)command
+- (void)changeIcon:(CDVInvokedUrlCommand*)command
 {
-  if (![self supportsAlternateIcons]) {
-    [self.commandDelegate sendPluginResult:[CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"This version of iOS doesn't support alternate icons"] callbackId:command.callbackId];
-    return;
-  }
-
-  NSDictionary* options = command.arguments[0];
-  NSString *iconName = options[@"iconName"];
-  BOOL suppressUserNotification = (options[@"suppressUserNotification"] == nil || [options[@"suppressUserNotification"] boolValue]);
-
-  if (iconName == nil) {
-    [self.commandDelegate sendPluginResult:[CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"The 'iconName' parameter is mandatory"] callbackId:command.callbackId];
-    return;
-  }
-
-  [[UIApplication sharedApplication] setAlternateIconName:iconName completionHandler:^(NSError *error) {
-      if (error != nil) {
-        NSString *errMsg = error.localizedDescription;
-        [self.commandDelegate sendPluginResult:[CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:errMsg] callbackId:command.callbackId];
-      } else {
-        [self.commandDelegate sendPluginResult:[CDVPluginResult resultWithStatus:CDVCommandStatus_OK] callbackId:command.callbackId];
-      }
-  }];
-
-
-  if (suppressUserNotification) {
-    [self suppressUserNotification];
-  }
+   if (@available(iOS 10.3, *)) {
+       if (![[UIApplication sharedApplication] supportsAlternateIcons]) {
+           CDVPluginResult* result =
+           [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR
+                             messageAsString:@"Alternate icons not supported"];
+           [self.commandDelegate sendPluginResult:result
+                                       callbackId:command.callbackId];
+           return;
+       }
+       NSDictionary* options = [command.arguments objectAtIndex:0];
+       NSString* iconName = options[@"iconName"];
+       dispatch_async(dispatch_get_main_queue(), ^{
+           [[UIApplication sharedApplication]
+            setAlternateIconName:iconName
+            completionHandler:^(NSError * _Nullable error) {
+               if (error) {
+                   NSLog(@"ICON ERROR: %@", error);
+                   CDVPluginResult* result =
+                   [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR
+                                     messageAsString:error.localizedDescription];
+                   [self.commandDelegate sendPluginResult:result
+                                               callbackId:command.callbackId];
+               } else {
+                   NSLog(@"ICON CHANGED");
+                   CDVPluginResult* result =
+                   [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
+                   [self.commandDelegate sendPluginResult:result
+                                               callbackId:command.callbackId];
+               }
+           }];
+       });
+   } else {
+       CDVPluginResult* result =
+       [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR
+                         messageAsString:@"iOS version not supported"];
+       [self.commandDelegate sendPluginResult:result
+                                   callbackId:command.callbackId];
+   }
 }
-
-
-#pragma mark - Helper functions
-
-- (BOOL) supportsAlternateIcons
-{
-    return [[UIApplication sharedApplication] supportsAlternateIcons];
-}
-
-- (void) suppressUserNotification
-{
-  UIViewController* suppressAlertVC = [UIViewController new];
-  [self.viewController presentViewController:suppressAlertVC animated:NO completion:^{
-      [suppressAlertVC dismissViewControllerAnimated:NO completion: nil];
-  }];
-}
-
 @end
